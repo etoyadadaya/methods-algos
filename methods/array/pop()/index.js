983b55ad96c14c2fa912f9593a6509136ee9123e
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-10-21 21:42:10 +0700

    own pop

--- methods/array/pop()/index.js
+++ methods/array/pop()/index.js
@@ -1,10 +1,22 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+// console.log(arr.pop());
+// console.log(arr);
 
 let arr = [1,2,3,4,5];
 
 Array.prototype.myPop = function () {
-
+    const value = this[this.length - 1];
+    this.length = this.length - 1;
+    return value;
 }
 
-console.log(arr.pop());
+console.log(arr.myPop());
+console.log(arr);
+console.log(arr.myPop());
+console.log(arr);
+console.log(arr.myPop());
+console.log(arr);
+console.log(arr.myPop());
+console.log(arr);
+console.log(arr.myPop());
 console.log(arr);
