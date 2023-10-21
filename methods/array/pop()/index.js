ebc65d3f69b942e45c4ccf1735e2ce3742996bbf
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-10-21 15:08:52 +0700

    own toReversed

---
+++ methods/array/pop()/index.js
@@ -0,0 +1,10 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+
+let arr = [1,2,3,4,5];
+
+Array.prototype.myPop = function () {
+
+}
+
+console.log(arr.pop());
+console.log(arr);
