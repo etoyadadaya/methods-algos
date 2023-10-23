491af4c5fa4a54539d37f0d324d2c82dc2fa20e0
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-10-23 10:18:16 +0700

    own values

---
+++ methods/array/values()/index.js
@@ -0,0 +1,15 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+
+const arr = [1,2,3,4,5];
+
+function myValues (arr) {
+    function* createGenerator() {
+        for (let i = 0; i < arr.length; i++) {
+            yield arr[i];
+        }
+    }
+    return createGenerator();
+}
+
+const valuesGenerator = myValues(arr);
+console.log(valuesGenerator.next());
---
+++ methods/object/valueOf()/index.js
@@ -0,0 +1,5 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/valueOf
+
+Object.prototype.myValueOf = function () {
+    console.log(1);
+}
